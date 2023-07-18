1eb1569d8bd6cf480249d74aafcbcb042f009cda
Chang Min Bark <changminbark@gmail.com>
2023-07-18 10:38:26 +0700

    Postwidget

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -3,6 +3,7 @@ import {
     FavoriteBorderOutlined, 
     FavoriteOutlined, 
     ShareOutlined,
+    AccountCircle,
     AccountCircleOutlined 
 } from "@mui/icons-material";
 import { 
@@ -76,7 +77,75 @@ const PostWidget = ({
     /* STOPPED AT 4:57:24, REMEMBER TO ADD ICON FOR USERS JOINED (USE ACCOUNTCIRCLEOUTLINED) */
     return (
         <WidgetWrapper m="2rem 0">
+            <Friend 
+                friendId={postUserId}
+                name={name}
+                subtitle={location}
+                userPicturePath={userPicturePath}
+            />
+            <Typography color={main} sx={{ mt: "1rem" }}>
+                {description}
+            </Typography>
+            {locationPicturePath && (
+                <img 
+                    width="100%"
+                    height="auto"
+                    alt="post"
+                    style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+                    src={`http://localhost:3001/assets/${locationPicturePath}`}
+                />
+            )}
+            <FlexBetween mt="0.25rem">
+                <FlexBetween gap="1rem">
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={patchLike}>
+                            {isLiked ? (
+                                <FavoriteOutlined sx={{ color: primary }} />
+                            ) : (
+                                <FavoriteBorderOutlined />
+                            )}
+                        </IconButton>
+                        <Typography>{likeCount}</Typography>
+                    </FlexBetween>
 
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={() => setIsComments(!isComments)}>
+                           <ChatBubbleOutlineOutlined /> 
+                        </IconButton>
+                        <Typography>{comments.length}</Typography>
+                    </FlexBetween>
+
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={patchUsers}>
+                            {isJoined ? (
+                                <AccountCircleOutlined sx={{ color: primary }} />
+                            ) : (
+                                <AccountCircle />
+                            )}
+                        </IconButton>
+                        <Typography>{joinCount}</Typography>
+                    </FlexBetween>
+
+                </FlexBetween>
+
+                <IconButton>
+                    <ShareOutlined />
+                </IconButton>
+            </FlexBetween>
+            {isComments && (
+                <Box mt="0.5rem">
+                    {/* Iterate through comment map. */}
+                    {comments.map((comment, i) => (
+                        <Box key={`${name}-${i}`}>
+                            <Divider />
+                            <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem"}}>
+                                {comment}
+                            </Typography>
+                        </Box>
+                    ))}
+                    <Divider />
+                </Box>
+            )}
         </WidgetWrapper>
     )
 };
