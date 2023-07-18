92dbd32aef730d1f961d8524233350fa6dd72914
Chang Min Bark <changminbark@gmail.com>
2023-07-18 11:07:34 +0700

    FriendListWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -31,7 +32,9 @@ const HomePage = () => {
                     <PostsWidget userId={_id} />
                 </Box>
                 {isNonMobileScreens && <Box flexBasis="26%"></Box>} 
-                <AdvertWidget />              
+                <AdvertWidget />
+                <Box m="2rem 0" />
+                <FriendListWidget userId={_id} />            
             </Box>
         </Box>
     );
