b00c5bae3cbbb4d31593df515d2ba767772f3804
Chang Min Bark <changminbark@gmail.com>
2023-08-09 00:50:56 +0700

    added joinpost route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 // ADD/MODIFY ROUTES HERE (and modify controllers) FOR OTHER POST FUNCTIONS IN WEBSITE (LIKE BOOKING?)
 import express from "express";
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { getFeedPosts, getUserPosts, joinPost, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -12,5 +12,6 @@ router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/join", verifyToken, joinPost);
 
 export default router;
